77f8472424fed213e650ed2d7b825dbf62ea6f59
Austin Hurst <mynameisaustinhurst@gmail.com>
2019-03-10 23:37:25 -0400

    Much faster startup, updates for modern klibs

--- experiment.py
+++ experiment.py
@@ -1,13 +1,15 @@
 __author__ = "Jon Mulle & Austin Hurst"
 
 import klibs
-from klibs.KLConstants import *
+from klibs.KLConstants import TIMEOUT, TK_S
 from klibs import P
-from klibs.KLUtilities import *
-from klibs.KLUserInterface import any_key
+from klibs.KLUtilities import deg_to_px
+from klibs.KLUserInterface import any_key, ui_request
 from klibs.KLGraphics import aggdraw_to_numpy_surface, fill, flip, blit, clear
-from klibs.KLCommunication import message, alert
+from klibs.KLCommunication import message
 from klibs.KLKeyMap import KeyMap
+from klibs.KLTime import CountDown
+from klibs.KLResponseCollectors import KeyPressResponse
 
 from PIL import Image, ImageDraw, ImageFilter
 import random
@@ -52,7 +54,6 @@ class FigureGroundSearch(klibs.Experiment):
     orientations = [0, 90, 180, 270]
 
     neutral_color = P.default_fill_color
-    pseudo_mask = None
     trial_start_msg = None
 
     #  trial vars
@@ -69,20 +70,14 @@ class FigureGroundSearch(klibs.Experiment):
     figure = None
     orientation = None
 
-    def __init__(self, *args, **kwargs):
-        super(FigureGroundSearch, self).__init__(*args, **kwargs)
 
     def setup(self):
         
         # Stimulus sizes
         
         self.stim_pad = deg_to_px(self.stim_pad)
-        padded_stim_size_px = deg_to_px(self.stim_size) + self.stim_pad + 1
         self.txtm.add_style('q_and_a', 48, WHITE)
-        
-        # Initialize keymap for recording responses
-        
-        self.keymap = KeyMap("FGSearch_response", ["z","/"], ["circle", "square"], [sdl2.SDLK_z, sdl2.SDLK_SLASH])
+    
         
         # Generate masks, stimuli, and fixations for the experiment
         
@@ -90,9 +85,8 @@ class FigureGroundSearch(klibs.Experiment):
         self.__generate_stimuli()
         self.__generate_fixations()
 
-        pseudo_mask = Image.new(RGBA, (padded_stim_size_px, padded_stim_size_px), NEUTRAL_COLOR)
-        self.pseudo_mask = aggdraw_to_numpy_surface(pseudo_mask)
         self.trial_start_msg = message("Press any key to advance...", 'default', blit_txt=False)
+
 
     def __generate_masks(self):
         smaller_than_screen = True
@@ -106,17 +100,25 @@ class FigureGroundSearch(klibs.Experiment):
             if size > self.maximum_mask_size:
                 e_str = "The maximum mask size this monitor can support is {0} degrees.".format(self.maximum_mask_size)
                 raise ValueError(e_str)
+
         clear()
-        message("Rendering masks...", "q_and_a", location=P.screen_c, registration=5, flip_screen=True)
+        msg = message("Rendering masks...", "q_and_a", blit_txt=False)
+        blit(msg, 5, P.screen_c)
+        flip()
+
         self.masks = {}
         for size in self.trial_factory.exp_factors['mask_size']:
-            pump()
-            self.masks["{0}_{1}".format(CENTRAL, size)] = self.mask(size, CENTRAL).render()
-            self.masks["{0}_{1}".format(PERIPHERAL, size)] = self.mask(size, PERIPHERAL).render()
+            ui_request()
+            self.masks["{0}_{1}".format(CENTRAL, size)] = self.render_mask(size, CENTRAL).render()
+            self.masks["{0}_{1}".format(PERIPHERAL, size)] = self.render_mask(size, PERIPHERAL).render()
+
 
     def __generate_stimuli(self):
         clear()
-        message("Generating stimuli...","q_and_a", location=P.screen_c, registration=5, flip_screen=True)
+        msg = message("Generating stimuli...", "q_and_a", blit_txt=False)
+        blit(msg, 5, P.screen_c)
+        flip()
+
         stimuli_labels = [[(False, 0), (CIRCLE, 0)], [(False, 90), (CIRCLE, 0)], [(False, 180), (CIRCLE, 0)],
                           [(False, 270), (CIRCLE, 0)],
                           [(False, 0), (SQUARE, 0)], [(False, 90), (SQUARE, 0)], [(False, 180), (SQUARE, 0)],
@@ -127,12 +129,14 @@ class FigureGroundSearch(klibs.Experiment):
                           [(SQUARE, 0), (False, 270)]]
 
         for sl in stimuli_labels:
-            stim = self.texture(sl[1][0], sl[1][1])
-            figure = self.figure(sl[0][0], sl[0][1])
-            stim.mask(figure, (0, 0))
+            stim = self.render_texture(sl[1][0], sl[1][1])
+            figure = self.render_figure(sl[0][0], sl[0][1])
+            stim.putalpha(figure)
+            stim = aggdraw_to_numpy_surface(stim)
             fig_text = sl[0][0] if sl[0][0] in (CIRCLE, SQUARE) else "D_%s" % sl[0][1]
             texture_text = sl[1][0] if sl[1][0] in (CIRCLE, SQUARE) else "D_%s" % sl[1][1]
             self.stimuli["{0}_{1}".format(fig_text, texture_text)] = stim.render()
+
 
     def __generate_fixations(self):
         
@@ -142,33 +146,20 @@ class FigureGroundSearch(klibs.Experiment):
         self.fixation_bottom  = (P.screen_x / 2,  3 * P.screen_y / 4)
         self.exp_meta_factors = {"fixation": [self.fixation_top, self.fixation_central, self.fixation_bottom]}
         
-        dc_box_size = 50 # Size of drift correct bounds in pixels
-        dc_bounds   = dc_box_size / 2
-        
-        # top drift correct box
-        dc_top_tl     = (self.fixation_top[0] - dc_bounds, self.fixation_top[1] - dc_bounds)
-        dc_top_br     = (self.fixation_top[0] + dc_bounds, self.fixation_top[1] + dc_bounds)
-        self.el.add_boundary('dc_top_box', [dc_top_tl, dc_top_br], RECT_BOUNDARY)
-        # central drift correct box
-        dc_central_tl = (self.fixation_central[0] - dc_bounds, self.fixation_central[1] - dc_bounds)
-        dc_central_br = (self.fixation_central[0] + dc_bounds, self.fixation_central[1] + dc_bounds)
-        self.el.add_boundary('dc_central_box', [dc_central_tl, dc_central_br], RECT_BOUNDARY)
-        # bottom drift correct box
-        dc_bottom_tl  = (self.fixation_bottom[0] - dc_bounds, self.fixation_bottom[1] - dc_bounds)
-        dc_bottom_br  = (self.fixation_bottom[0] + dc_bounds, self.fixation_bottom[1] + dc_bounds)
-        self.el.add_boundary('dc_bottom_box', [dc_bottom_tl, dc_bottom_br], RECT_BOUNDARY)
 
     def block(self):
         pass
     
+
     def setup_response_collector(self):
         self.rc.display_callback = self.screen_refresh
         self.rc.terminate_after = [self.search_time, TK_S]
-        self.rc.uses([RC_KEYPRESS])
+        self.rc.uses([KeyPressResponse])
         self.rc.keypress_listener.interrupts = True
-        self.rc.keypress_listener.key_map = self.keymap
+        self.rc.keypress_listener.key_map = {'z': "circle", '/': "square"}
         self.rc.flip = False # Disable flipping on each rc loop, since callback already flips
     
+
     def trial_prep(self):
         clear()
         # choose randomly varying parts of trial
@@ -186,12 +177,13 @@ class FigureGroundSearch(klibs.Experiment):
         self.mask_label = "{0}_{1}".format(self.mask_type, self.mask_size)
         try:
             self.mask = self.masks[self.mask_label]
-        except KeyError as e:
+        except KeyError:
             self.mask = None  # for the no mask condition, easier than creating empty keys in self.masks
         blit(self.trial_start_msg, 5, P.screen_c)
         flip()
         any_key()
         self.el.drift_correct(self.fixation)
+
 
     def trial(self):
         """
@@ -207,9 +199,14 @@ class FigureGroundSearch(klibs.Experiment):
             print(resp)
 
         # handle timeouts
-        if resp[0] == TIMEOUT:
-            clear()
-            alert("Too slow!", False, 1)
+        if resp.rt == TIMEOUT:
+            feedback = CountDown(1)
+            msg = message("Too slow!", "alert", blit_txt=False)
+            while feedback.counting():
+                ui_request()
+                fill()
+                blit(msg, 5, P.screen_c)
+                flip()
             clear()
 
         #  create readable data as fixation is currrently in (x,y) coordinates
@@ -222,8 +219,8 @@ class FigureGroundSearch(klibs.Experiment):
             initial_fixation = "BOTTOM"
 
         return {"practicing": P.practicing,
-                "response": resp[0],
-                "rt": float(resp[1]),
+                "response": resp.value,
+                "rt": float(resp.rt),
                 "mask_type": self.mask_type,
                 "mask_size": self.mask_size,
                 "local": self.target_shape if self.target_level == LOCAL else "D",
@@ -233,21 +230,23 @@ class FigureGroundSearch(klibs.Experiment):
                 "block_num": P.block_number,
                 "initial_fixation": initial_fixation}
 
+
     def trial_clean_up(self):
         self.fixation = None
+
 
     def clean_up(self):
         pass
 
-    def texture(self, texture_figure, orientation=None):
+
+    def render_texture(self, texture_figure, orientation=None):
         grid_size = (deg_to_px(self.stim_size) + self.stim_pad) * 2
         stim_offset = self.stim_pad // 2
-        dc = Image.new(RGBA, (grid_size, grid_size), (0,0,0,0))
+        dc = Image.new('RGB', (grid_size, grid_size), NEUTRAL_COLOR[:3])
         stroke_width = 2  #px
         grid_cell_size = deg_to_px(self.bg_element_size + self.bg_element_pad)
         grid_cell_count = grid_size // grid_cell_size
         stim_offset += (grid_size % grid_cell_size) // 2  # split grid_size %% cells over pad
-
 
         # Visual Representation of the Texture Rendering Logic
         # <-------G-------->
@@ -264,7 +263,7 @@ class FigureGroundSearch(klibs.Experiment):
         element_offset = self.bg_element_pad // 2  # so as to apply padding equally on all sides of bg elements
         for col in range(0, grid_cell_count):
             for row in range(0, grid_cell_count):
-                pump()
+                ui_request()
                 top_out = int(row * grid_cell_size + element_offset + stim_offset)
                 top_in = top_out + stroke_width  # ie. top_inner
                 left_out = int(col * grid_cell_size + element_offset + stim_offset)
@@ -275,25 +274,28 @@ class FigureGroundSearch(klibs.Experiment):
                 right_in = right_out - stroke_width
 
                 if texture_figure == CIRCLE:
-                    ImageDraw.Draw(dc, RGBA).ellipse((left_out, top_out, right_out, bottom_out), WHITE)
-                    ImageDraw.Draw(dc, RGBA).ellipse((left_in, top_in, right_in, bottom_in), NEUTRAL_COLOR)
-                elif texture_figure == SQUARE:
-                    ImageDraw.Draw(dc, RGBA).rectangle((left_out, top_out, right_out, bottom_out), WHITE)
-                    ImageDraw.Draw(dc, RGBA).rectangle((left_in, top_in, right_in, bottom_in), NEUTRAL_COLOR)
-                elif texture_figure is False:
+                    ImageDraw.Draw(dc, 'RGB').ellipse((left_out, top_out, right_out, bottom_out), WHITE[:3])
+                    ImageDraw.Draw(dc, 'RGB').ellipse((left_in, top_in, right_in, bottom_in), NEUTRAL_COLOR[:3])
 
+                elif texture_figure == SQUARE:
+                    ImageDraw.Draw(dc, 'RGB').rectangle((left_out, top_out, right_out, bottom_out), WHITE[:3])
+                    ImageDraw.Draw(dc, 'RGB').rectangle((left_in, top_in, right_in, bottom_in), NEUTRAL_COLOR[:3])
+
+                elif texture_figure is False:
                     half_el_width = int(0.5 * deg_to_px(self.bg_element_size))
                     rect_right = right_out - half_el_width
-                    ImageDraw.Draw(dc, RGBA, ).ellipse((left_out, top_out, right_out, bottom_out), WHITE)
-                    ImageDraw.Draw(dc, RGBA, ).ellipse((left_in, top_in, right_in, bottom_in), NEUTRAL_COLOR)
-                    ImageDraw.Draw(dc, RGBA, ).rectangle((left_out, top_out, rect_right, bottom_out), WHITE)
-                    ImageDraw.Draw(dc, RGBA, ).rectangle((left_in, top_in, rect_right, bottom_in), NEUTRAL_COLOR)
+                    ImageDraw.Draw(dc, 'RGB', ).ellipse((left_out, top_out, right_out, bottom_out), WHITE[:3])
+                    ImageDraw.Draw(dc, 'RGB', ).ellipse((left_in, top_in, right_in, bottom_in), NEUTRAL_COLOR[:3])
+                    ImageDraw.Draw(dc, 'RGB', ).rectangle((left_out, top_out, rect_right, bottom_out), WHITE[:3])
+                    ImageDraw.Draw(dc, 'RGB', ).rectangle((left_in, top_in, rect_right, bottom_in), NEUTRAL_COLOR[:3])
+
         dc = dc.resize((grid_size // 2, grid_size // 2), Image.ANTIALIAS)
         dc = dc.rotate(orientation)
 
-        return aggdraw_to_numpy_surface(dc)
+        return dc
 
-    def figure(self, figure_shape, orientation):
+
+    def render_figure(self, figure_shape, orientation):
 
         stim_size_px = deg_to_px(self.stim_size)
         half_pad = self.stim_pad
@@ -302,25 +304,24 @@ class FigureGroundSearch(klibs.Experiment):
         br_fig = 2 * stim_size_px
         rect_right = br_fig - stim_size_px
         dc_size = (stim_size_px + half_pad) * 2
-        dc = Image.new(RGBA,  (dc_size, dc_size), (0,0,0,0))
+        dc = Image.new('L', (dc_size, dc_size), 0)
 
         if figure_shape == CIRCLE:
-            ImageDraw.Draw(dc, RGBA).ellipse((tl_fig, tl_fig, br_fig, br_fig), WHITE)
+            ImageDraw.Draw(dc, 'L').ellipse((tl_fig, tl_fig, br_fig, br_fig), 255)
         if figure_shape == SQUARE:
-            ImageDraw.Draw(dc, RGBA).rectangle((tl_fig, tl_fig, br_fig, br_fig), WHITE)
+            ImageDraw.Draw(dc, 'L').rectangle((tl_fig, tl_fig, br_fig, br_fig), 255)
         if figure_shape is False:
-            ImageDraw.Draw(dc, RGBA).ellipse((tl_fig, tl_fig, br_fig, br_fig), WHITE)
-            ImageDraw.Draw(dc, RGBA).rectangle((tl_fig, tl_fig, rect_right, br_fig), WHITE)
-        if orientation > 0: dc = dc.rotate(orientation)
-        cookie_cutter = aggdraw_to_numpy_surface(dc.resize((dc_size // 2, dc_size // 2), Image.ANTIALIAS))
-        dough = Image.new(RGBA,  (dc_size // 2, dc_size // 2), WHITE)
-        ImageDraw.Draw(dough, RGBA).rectangle((0, 0, dc_size // 2, dc_size // 2), WHITE)
-        dough = aggdraw_to_numpy_surface(dough)
-        dough.mask(cookie_cutter, (0, 0))
+            ImageDraw.Draw(dc, 'L').ellipse((tl_fig, tl_fig, br_fig, br_fig), 255)
+            ImageDraw.Draw(dc, 'L').rectangle((tl_fig, tl_fig, rect_right, br_fig), 255)
 
-        return dough
+        if orientation > 0:
+            dc = dc.rotate(orientation)
+        cookie_cutter = dc.resize((dc_size // 2, dc_size // 2), Image.ANTIALIAS)
 
-    def mask(self, diameter, mask_type):
+        return cookie_cutter
+
+
+    def render_mask(self, diameter, mask_type):
         MASK_COLOR = NEUTRAL_COLOR
         diameter = deg_to_px(diameter)
         blur_width = self.mask_blur_width
@@ -358,6 +359,7 @@ class FigureGroundSearch(klibs.Experiment):
 
         return mask
 
+
     def screen_refresh(self):
         position = self.el.gaze()
         fill()
@@ -368,4 +370,3 @@ class FigureGroundSearch(klibs.Experiment):
             if self.mask is not None:
                 blit(self.mask, 5, position)
         flip()
-
